c6fd412dade5d772d7b6f8feed7394b02aee1ca7
Hunter Kassow <hunterkassow@protonmail.com>
2022-06-08 09:31:33 -0400

    commit tree b378f8d4f90dcf9275d3a609ad93eb53ac045682

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+import React, { useState } from "react"
+;
+const Keypad = () => {
+    const [passwordIs, setPasswordIs] = useState('')
+    function changer(e){
+        setPasswordIs(e.target.value)
+        console.log('Entering password...')
+        console.log(e.target.value)
+    }
+    return (
+        <input onChange={changer} type="password" />
+    )
+}
+
+export default Keypad
